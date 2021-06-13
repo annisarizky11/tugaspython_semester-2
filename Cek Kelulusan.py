7a94e03cf9d26eaedde7c94bd1b0a18748a7ccb1
Annisa Rizky Ramdhani <56909854+annisarizky11@users.noreply.github.com>
2021-06-13 22:57:40 +0700

    Pembahasan penggunaan if, else, elif, while,and or
    Pembahasan penggunaan if.else,elif,while, and or

---
+++ Cek Kelulusan.py
@@ -0,0 +1,24 @@
+"""
+ANNISA RIZKY RAMDHANI - 20083000076 - 2C
+12-06-2021
+PROGRAM CEK KELULUSAN
+"""
+#cek kelulusan, jika nilai > 60 maka status lulus
+jwb = "y"
+while jwb=="y" or jwb=="Y":
+    print ("==========================")
+    print (" CEK KELULUSAN ")
+    print ("==========================")
+    #setiap value yang diinputkan, secara default bertipe data STRING
+    n = input(">> Masukkan Nilai = ")
+    #cek nilai
+    if n>60:
+        sts = "LULUS"
+    else:
+        sts="ULANG"
+    print(sts)
+
+    #inputan untuk break
+    jwb = input(">> Mulai lagi ? y/t = ")
+    if jwb=="t" or jwb=="T":
+        break
